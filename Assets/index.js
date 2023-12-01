d3d17a02370d732c108c9560309806bc143d7bad
Jess Skelton <jessskelton888@gmail.com>
2023-11-30 19:58:14 -0600

    updated checkbox button on HTML by andrue and Jess updated checkbox functionality inside JS code

--- Assets/index.js
+++ Assets/index.js
@@ -1,42 +1,47 @@
 // DOM Elements and Global Variables
 const apiKey = 'd21c891cc7d24269b62de05633d46e54';
 const searchInput = document.querySelector('input'); // Gets the user input from the search field
+const searchForm = document.getElementById('searchForm'); // Gets the form element
 const resultsContainer = document.getElementById('foodContent'); // Gets the container to display the recipe divs
 
 // Function to only run once the document is fully loaded and ready
 document.addEventListener('DOMContentLoaded', function () {
 
-    // Event listener for ingredient checkboxes AND search bar typed input
-    searchInput.addEventListener('input', searchRecipes);
+    // Function to run when the search button is clicked
+    function onSearchButtonClick(event) {
+        event.preventDefault(); // Prevents the form from submitting
+        searchRecipes();
+    }
+
+    // Event listener for the form button click
+    searchForm.addEventListener('click', onSearchButtonClick);
+
+    // Event listener for ingredient checkboxes 
     document.querySelectorAll('input[type=checkbox]').forEach(checkbox => {
-        checkbox.addEventListener('change', searchRecipes);
+        checkbox.addEventListener('change', onSearchButtonClick);
     });
 
     // Function to actually run searched Recipes 
     function searchRecipes() {
-
-        const searchTerm = searchInput.value.trim(); // Trim search recipe name
         const selectedIngr = Array.from(document.querySelectorAll('input[type=checkbox]:checked')).map(checkbox => checkbox.value); // Array of selected ingredients from checkboxes
+        const searchTerm = searchInput.value.trim(); // Trim search recipe name
 
-        // Make sure that results only pull if something is typed
-        if (searchTerm !== '') {
-
+        // Make sure that results only pull if something is checked or typed
+        if (selectedIngr.length > 0 || searchTerm !== '') {
             // Spoonacular API URL Request to search recipe name with Appetizer & only 6 result parameters
             const apiUrl = `https://api.spoonacular.com/recipes/search?apiKey=${apiKey}&query=${searchTerm}&type=appetizer&number=6`;
 
             if (selectedIngr.length > 0) {
-
                 const ingredientsQuery = selectedIngr.map(ingredient => `&includeIngredients=${ingredient}`).join(''); // Query for selected checkboxes
-                
+
                 // Fetch API data along with checkboxes & query parameters
                 fetch(apiUrl + ingredientsQuery)
                     .then(response => response.json())
                     .then(data => {
-
                         // Clear any existing results
                         resultsContainer.innerHTML = '';
 
-                        // Diplay each recipe title in a seperate div as 6 different divs
+                        // Display each recipe title in a separate div as 6 different divs
                         data.results.forEach(result => {
                             const recipeElement = document.createElement('div');
                             recipeElement.classList.add('box');
@@ -45,16 +50,14 @@ document.addEventListener('DOMContentLoaded', function () {
                         });
                     });
             } else {
-
                 // Fetch API data without the checkboxes 
                 fetch(apiUrl)
                     .then(response => response.json())
                     .then(data => {
-
                         // Clear any existing results
                         resultsContainer.innerHTML = '';
 
-                        // Display each recipe title in a seperate div as 6 different divs
+                        // Display each recipe title in a separate div as 6 different divs
                         data.results.forEach(result => {
                             const recipeElement = document.createElement('div');
                             recipeElement.classList.add('box');
@@ -64,8 +67,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     });
             }
         } else {
-
-            // If search is empty, clear results container 
+            // If no checkboxes are checked and the search is empty, clear results container 
             resultsContainer.innerHTML = '';
         }
     }
