77254e324b7739b89e37c1142ed2b52ca5d8baf3
AndrueGage <trainer.andrue@gmail.com>
2023-12-06 00:45:07 -0600

    aside for local storage, localstoragedisplay js

--- Assets/index.js
+++ Assets/index.js
@@ -1,7 +1,5 @@
 // DOM Elements and Global Variables
-
-const apiKey = '2f0fec85982749e2b27c11e993695c3d';
-
+const apiKey = '1b4033d0fab74f9baa5f8b0d1950e612';
 
 const searchInput = document.querySelector('input'); // Gets the user input from the search field
 const searchForm = document.getElementById('searchForm'); // Gets the form element
@@ -10,151 +8,180 @@ const savedEntriesDiv = document.getElementById('savedEntriesDiv');
 const modal = document.getElementById('default-modal');
 const toggleBtn = document.getElementById('toggleBtn');
 
-const dropdownList = document.getElementById('dropdownList');
+// Event listener for the form button click
+searchForm.addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevents the form from submitting
+    searchRecipes();
+})
 
-// Function to only run once the document is fully loaded and ready
-// document.addEventListener('DOMContentLoaded', function () {
-
-    // Event listener for the form button click
-    searchForm.addEventListener('submit', function (event) {
-        event.preventDefault(); // Prevents the form from submitting
-        searchRecipes();
-        
-    // Get the search term from the input field
-    const searchTerm = searchInput.value.trim();
-    })
-
-    searchInput.addEventListener('focus', function() {
-        dropdownList.style.display = 'block';
-      });
-      
-      searchInput.addEventListener('blur', function() {
-        dropdownList.style.display = 'none';
-      });
-      
-      // Listen for click events on the dropdown list items
-      dropdownList.addEventListener('click', function(event) {
-        if (event.target.tagName === 'LI') {
-            searchInput.value = event.target.textContent; // Set input value to clicked item
-            dropdownList.style.display = 'none'; // Hide the dropdown
-        }
-      });
-
-    function displayHomePage(data) {
-        data.results.forEach(result => {
-            const recipeElement = document.createElement('div');
-            recipeElement.addEventListener('click', () => displaySingleRecipe(result.id));
-            recipeElement.classList.add('cursor-pointer');
-            const h3 = document.createElement('h3');
-            const img = document.createElement('img');
-            h3.textContent = result.title;
-            img.src = result.image;
-            h3.classList.add('word-wrapper');
-            recipeElement.classList.add('recipe-container');
-            recipeElement.appendChild(h3);
-            recipeElement.appendChild(img);
-            resultsContainer.appendChild(recipeElement);
-        })
-    }
-    async function displaySingleRecipe(id) {
-        const data = await fetchSingleRecipe(id);
-
-        // Save id/recipe name to local storage
-        saveToLocalStorage(data.title);
-
-        const title = document.getElementById('recipeTitle');
-        title.textContent = '';
-        title.textContent = data.title;
-        const box = document.getElementById('recipeDesc');
-        box.innerHTML = '';
+function displayHomePage(data) {
+    data.results.forEach(result => {
+        const recipeElement = document.createElement('div');
+        recipeElement.addEventListener('click', () => displaySingleRecipe(result.id));
+        recipeElement.classList.add('cursor-pointer');
+        const h3 = document.createElement('h3');
         const img = document.createElement('img');
-        img.src = data.image;
-        img.classList.add('w-full', 'rounded-lg');
-        box.appendChild(img);
-        const summary = document.createElement('p');
-        summary.innerHTML = data.summary;  
-        const instructions = document.createElement('div');
-        instructions.innerHTML = data.instructions;
-        const servingSize = document.getElementById('servings');
-        const userScore = document.getElementById('userScore');
-        const readyTime = document.getElementById('readyTime');
-        servingSize.textContent = data.servings;
-        userScore.textContent = data.spoonacularScore.toFixed();
-        readyTime.textContent = data.readyInMinutes;
-        box.appendChild(summary);
-        box.appendChild(instructions);
-        toggleBtn.click();
+        h3.textContent = result.title;
+        img.src = result.image;
+        h3.classList.add('word-wrapper');
+        img.classList.add('rounded-lg')
+        recipeElement.classList.add('recipe-container');
+        recipeElement.appendChild(h3);
+        recipeElement.appendChild(img);
+        resultsContainer.appendChild(recipeElement);
+    })
+}
+function removeFirstATagAndAfter(htmlString) {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(htmlString, 'text/html');
+
+    // Find the first <a> tag
+    const firstATag = doc.querySelector('a');
+    
+    if (firstATag) {
+        // Remove everything after and including the first <a> tag
+        const parent = firstATag.parentNode;
+        let currentNode = firstATag;
+        while (currentNode) {
+            const nextNode = currentNode.nextSibling;
+            parent.removeChild(currentNode);
+            currentNode = nextNode;
+        }
     }
-    // Function to save recipie name into local storage
-    function saveToLocalStorage(recipeName) {
-        const savedEntries = JSON.parse(localStorage.getItem('SavedEntries')) || [];
-        // Check if the entry already exists in the local storage
-        const existingEntryIndex = savedEntries.findIndex(entry => entry === recipeName);
-        if (existingEntryIndex !== -1) {
+
+    // Get the modified HTML string
+    const modifiedHtmlString = doc.body.innerHTML;
+
+    return modifiedHtmlString;
+}
+
+async function displaySingleRecipe(id) {
+    const data = await fetchSingleRecipe(id);
+
+    // Save id/recipe name to local storage
+    saveToLocalStorage(data.title, data.id);
+
+    const title = document.getElementById('recipeTitle');
+    title.textContent = '';
+    title.textContent = data.title;
+    const box = document.getElementById('recipeDesc');
+    box.innerHTML = '';
+    const img = document.createElement('img');
+    img.src = data.image;
+    img.classList.add('w-full', 'rounded-lg');
+    box.appendChild(img);
+    const summary = document.createElement('p');
+    summary.innerHTML = removeFirstATagAndAfter(data.summary);
+    const instructions = document.createElement('div');
+    instructions.innerHTML = data.instructions;
+    const servingSize = document.getElementById('servings');
+    const userScore = document.getElementById('userScore');
+    const readyTime = document.getElementById('readyTime');
+    servingSize.textContent = data.servings;
+    userScore.textContent = data.spoonacularScore.toFixed();
+    readyTime.textContent = data.readyInMinutes;
+    box.appendChild(summary);
+    box.appendChild(instructions);
+    toggleBtn.click();
+}
+function displayLocalStorage() {
+    const localStorageDis = document.getElementById('displayLocalStorage');
+    const savedEntries = JSON.parse(localStorage.getItem('SavedEntries'));
+    if (savedEntries.length > 0) {
+        localStorageDis.classList.remove('hidden');
+        localStorageDis.innerHTML = '';
+        const h3 = document.createElement('h3');
+        h3.textContent = 'Recently Viewed Recipes';
+        h3.classList.add('text-lg', 'font-bold')
+        localStorageDis.appendChild(h3);
+        savedEntries.forEach(entry => {
+            const p = document.createElement('p');
+            p.textContent = entry.recipeName;
+            p.addEventListener('click', () => displaySingleRecipe(entry.id));
+            p.classList.add('cursor-pointer', 'word-wrapper')
+            localStorageDis.appendChild(p);
+        });
+    }
+}
+// Function to save recipie name into local storage
+function saveToLocalStorage(recipeName, id) {
+    const savedEntries = JSON.parse(localStorage.getItem('SavedEntries')) || [];
+    // Check if the entry already exists in the local storage
+    const existingEntryIndex = savedEntries.findIndex(entry => entry.recipeName === recipeName);
+    if (existingEntryIndex !== -1) {
         // Remove the existing entry to move it to the top
         savedEntries.splice(existingEntryIndex, 1);
-        }
-        // Add the new entry to the beginning of the array
-        savedEntries.unshift(recipeName);
-
-        // Save the updated array back to local storage
-        localStorage.setItem('SavedEntries', JSON.stringify(savedEntries));
     }
-    // Function to actually run searched Recipes 
-    async function searchRecipes() {
-        const checkboxes = Array.from(document.querySelectorAll('input[type=checkbox]:checked')).map(checkbox => checkbox.value);
-        const selectedIngr = checkboxes.length > 0 ? checkboxes : [];
-        const searchTerm = searchInput.value.trim(); // Trim search recipe name
-        resultsContainer.innerHTML = '';
-        if (selectedIngr.length > 0) {
-            const data = await fetchSearchAndIng(searchTerm, selectedIngr);
-            displayHomePage(data);
-        } else {
-            const data = await fetchSearchTerm(searchTerm);
-            displayHomePage(data);
-        }
-
+    if (savedEntries.length == 5) {
+        savedEntries.pop();
     }
-    async function fetchSingleRecipe(id) {
-        const URL = `https://api.spoonacular.com/recipes/${id}/information?includeNutrition=false&apiKey=${apiKey}`
-        try {
-            const response = await fetch(URL);
-            const data = await response.json();
-
-            return data;
-        } catch (error) {
-            console.error('Error fetching URL', error.message);
-        }
+    let tempObject = {
+        id: id,
+        recipeName: recipeName,
     }
-    async function fetchSearchTerm(searchTerm) {
-        const URL = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${searchTerm}&type=appetizer&number=6`
-        try {
-            const response = await fetch(URL);
-            const data = await response.json();
+    // Add the new entry to the beginning of the array
+    savedEntries.unshift(tempObject);
 
-            return data;
-        } catch (error) {
-            console.error('Error fetching URL', error.message);
-        }
-    }
-    async function fetchSearchAndIng(searchTerm, ingredients) {
-        const URL = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${searchTerm}&type=appetizer&number=6&includeIngredients=${ingredients}`
-        try {
-            const response = await fetch(URL);
-            const data = await response.json();
-
-            return data;
-        } catch (error) {
-            console.error('Error fetching URL', error.message);
-        }
-    }
-    async function init() {
-        let loading = document.createElement('p');
-        resultsContainer.appendChild(loading);
-        loading.textContent = 'Loading...';
-        const data = await fetchSearchTerm('wings');
-        resultsContainer.removeChild(loading);
+    // Save the updated array back to local storage
+    localStorage.setItem('SavedEntries', JSON.stringify(savedEntries));
+    displayLocalStorage()
+}
+// Function to actually run searched Recipes 
+async function searchRecipes() {
+    const checkboxes = Array.from(document.querySelectorAll('input[type=checkbox]:checked')).map(checkbox => checkbox.value);
+    const selectedIngr = checkboxes.length > 0 ? checkboxes : [];
+    const searchTerm = searchInput.value.trim(); // Trim search recipe name
+    resultsContainer.innerHTML = '';
+    if (selectedIngr.length > 0) {
+        const data = await fetchSearchAndIng(searchTerm, selectedIngr);
+        displayHomePage(data);
+    } else {
+        const data = await fetchSearchTerm(searchTerm);
         displayHomePage(data);
     }
-    init()
-// })
+
+}
+async function fetchSingleRecipe(id) {
+    const URL = `https://api.spoonacular.com/recipes/${id}/information?includeNutrition=false&apiKey=${apiKey}`
+    try {
+        const response = await fetch(URL);
+        const data = await response.json();
+
+        return data;
+    } catch (error) {
+        console.error('Error fetching URL', error.message);
+    }
+}
+async function fetchSearchTerm(searchTerm) {
+    const URL = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${searchTerm}&type=appetizer&number=6`
+    try {
+        const response = await fetch(URL);
+        const data = await response.json();
+
+        return data;
+    } catch (error) {
+        console.error('Error fetching URL', error.message);
+    }
+}
+async function fetchSearchAndIng(searchTerm, ingredients) {
+    const URL = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${searchTerm}&type=appetizer&number=6&includeIngredients=${ingredients}`
+    try {
+        const response = await fetch(URL);
+        const data = await response.json();
+
+        return data;
+    } catch (error) {
+        console.error('Error fetching URL', error.message);
+    }
+}
+async function init() {
+    let loading = document.createElement('p');
+    resultsContainer.appendChild(loading);
+    loading.textContent = 'Loading...';
+    const data = await fetchSearchTerm('wings');
+    resultsContainer.removeChild(loading);
+    displayHomePage(data);
+    displayLocalStorage();
+}
+init()
+
